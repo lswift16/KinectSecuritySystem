72bd31d7c88909c269c171c549e9694c225c10b5
TomWild259 <tom-wild@hotmail.co.uk>
2017-03-17 18:53:30 +0000

    Latest arduino code

--- Assignment_2/Assignment_2.ino
+++ Assignment_2/Assignment_2.ino
@@ -1,85 +1,63 @@
 //Group Best
-#include <ZumoMotors.h>
-#include <NewPing.h>
 #include <Servo.h> 
-
-#define SPEED 100             //Sets motor speed
-#define STOP 0
-#define REVERSE_DURATION  200 // ms
-#define TURN_DURATION     100 // ms - 300 before
-#define TRIGGER_PIN 2
-#define ECHO_PIN 12
-#define MAX_DISTANCE 200
  
-Servo middle;  // creates 4 "servo objects"
-ZumoMotors motors;
+Servo middle, claw, left, right;    //Declare servos
 
-const int ledPin = 13; // the pin that the LED is attached to
-int incomingByte;      // a variable to read incoming serial data into
+int incomingByte;
+int middlePosition = 90;
+int leftRightPosition= 90;
+int clawPosition;
 
 void setup() {
   Serial.begin(9600);               //Initialise serial 
-  middle.attach(6);  // attaches the servo on pin 11 to the middle object
-  pinMode(9, OUTPUT);
-  beep(50);
-  beep(50);
-  beep(50);
-  delay(1000);
-  
-  //pinMode(ledPin, OUTPUT);          //Initialise the LED pin as an output
-  //sensors.init();                   //Initialise sensors
-
+  middle.attach(6);                 //Attach servos to pins
+  claw.attach(9);
+  left.attach(11);
+  right.attach(10);
 }
 
 void loop() {  
-    //controlServo();
-    //middle.write(90); // sets the servo position according to the value(degrees)
-    //delay(300); // doesn't constantly update the servos which can fry them
-
-    if (Serial.available() > 0) {     //Checks if serial input is available
-      incomingByte = Serial.read();   //Reads input
-      switch(incomingByte)            //Checks value of incomingByte
+    
+    if (Serial.available() > 0) {                 //Checks if serial input is available
+      incomingByte = Serial.read();               //Get letter from serial
+      Serial.read();                              //Get comma from serial
+      int incomingPosition = Serial.parseInt();   //Get int from serial for position
+      //Serial.read();                            //Get comma
+      //int incomingYPosition = Serial.parseInt();//Get int for Y position
+      
+      switch(incomingByte)
       {
-        case 'U':
-           middle.write(90); // sets the servo position according to the value(degrees)
-           delay(50); // doesn't constantly update the servos which can fry them
-           beep(200);
+        case 'X':                                              //Sets the middle servo position
+            middle.write(incomingPosition);
+            //left.write(incomingYPosition);
+            //right.write(incomingYPosition);
+            delay(15);
           break;
-        case'L':
-           middle.write(-90); // sets the servo position according to the value(degrees)
-           delay(300); // doesn't constantly update the servos which can fry them
+        case'Y':                                                //Move the right and left servos
+            left.write(incomingPosition);
+            right.write(incomingPosition);
           break;
-      }  
-      incomingByte = Serial.read();
-      incomingByte = 0;
+        /*case 'R':                                             //Turn right from 180 -> 0
+           for(int i = middlePosition; i>=incomingPosition; i-= 1)
+           {
+              middle.write(i);
+              delay(15);
+              middlePosition = incomingPosition;
+           }
+          break;
+        case'U':                                                //Move arm up
+           for(int i = leftRightPosition; i>=incomingPosition; i-= 1)
+           {
+              left.write(i);
+              right.write(i);
+              delay(15);
+              leftRightPosition = incomingPosition;
+           }
+          break;*/
+
+      }
     }
 }
-
-void controlServo() {                //For controlling zumo
-  if (Serial.available() > 0) {     //Checks if serial input is available
-    incomingByte = Serial.read();   //Reads input
-    switch(incomingByte)            //Checks value of incomingByte
-    {
-      case 'U':
-         middle.write(90); // sets the servo position according to the value(degrees)
-         delay(50); // doesn't constantly update the servos which can fry them
-        break;
-      case'L':
-         middle.write(-90); // sets the servo position according to the value(degrees)
-         delay(300); // doesn't constantly update the servos which can fry them
-        break;
-    }  
-    incomingByte = Serial.read();
-    incomingByte = 0;
-  }
-}
-void beep(unsigned char delayms){
-  analogWrite(9, 20);      // Almost any value can be used except 0 and 255
-                           // experiment to get the best tone
-  delay(delayms);          // wait for a delayms ms
-  analogWrite(9, 0);       // 0 turns it off
-  delay(delayms);          // wait for a delayms ms   
-} 
 
 
 
